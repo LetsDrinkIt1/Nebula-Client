099738aacb9d0e745523fc30e5a6dc003a44ce70
LetsDrinkIt1 <letsdrinkit406@gmail.com>
2026-08-05 03:59:32 +0100

    Update bot status version to 0.9

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Under Development! V: B: 0.8",     // this is what people actually see
+        state: "Under Development! V: B: 0.9",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
